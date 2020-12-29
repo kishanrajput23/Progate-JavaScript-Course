f4cd5d034ce7b0db1ec3c821ee3beec6a564f727
Kishan Kumar Rai <70385488+kishanrajput23@users.noreply.github.com>
2020-12-29 20:36:33 +0530

    Add files via upload

---
+++ javascript_study_1/page18/script.js
@@ -0,0 +1,18 @@
+const rank = 2;
+
+switch (rank) {
+  case 1:
+    console.log("You won a gold medal!");
+    break;
+
+  // Add a case for when rank is 2
+  case 2:
+    console.log("You won a silver medal!");
+    break;
+  
+  // Add a case for when rank is 3
+  case 3:
+    console.log("You won a bronze medal!");
+    break;
+  
+}
